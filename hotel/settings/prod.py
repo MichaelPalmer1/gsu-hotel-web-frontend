d0d7d409a454da8aaa2c59a92e1f9bc8f8874e5c
Michael Palmer <michael@michaeldpalmer.com>
2016-04-06 21:48:52 +0000

    Update hostname

--- hotel/settings/prod.py
+++ hotel/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # MySQL configuration
-MYSQL_DATABASE_HOST = 'michaeldpalmer.com'
+MYSQL_DATABASE_HOST = 'db.michaeldpalmer.com'
 MYSQL_DATABASE_DB = 'hotel'
 MYSQL_DATABASE_USER = 'hotel'
 MYSQL_DATABASE_PASSWORD = 'ZfDh3wpDRBZbmUmC'
