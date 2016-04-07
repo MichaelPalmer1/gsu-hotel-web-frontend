ba96be2afddd4a24d61798e3d47d049bc5feac87
Michael Palmer <michael@michaeldpalmer.com>
2016-04-07 18:50:22 -0400

    Updated prod settings, modified docker build, and renamed hotel.wsgi to run.py (not really using it as a wsgi anyways)

--- hotel/settings/prod.py
+++ hotel/settings/prod.py
@@ -1,7 +1,14 @@
 from .base import *
+from .setting_utils import _init_app_db
 
 # MySQL configuration
-MYSQL_DATABASE_HOST = 'db.michaeldpalmer.com'
-MYSQL_DATABASE_DB = 'hotel'
-MYSQL_DATABASE_USER = 'hotel'
-MYSQL_DATABASE_PASSWORD = 'ZfDh3wpDRBZbmUmC'
+DATABASE = {
+    'MYSQL_HOST': 'db.michaeldpalmer.com',
+    'MYSQL_DB': 'hotel',
+    'MYSQL_USER': 'hotel',
+    'MYSQL_PASSWORD': 'ZfDh3wpDRBZbmUmC'
+}
+
+_init_app_db(mysql, app, DATABASE)
+
+registered_tables = ['asset']
