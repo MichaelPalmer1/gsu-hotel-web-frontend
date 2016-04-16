d8e61f565a19bfb6e68c6ede4f7bfcf586e8f341
Michael Palmer <michael@michaeldpalmer.com>
2016-04-16 22:51:04 +0000

    Changing hostname to link with the mysql docker

--- hotel/settings/prod.py
+++ hotel/settings/prod.py
@@ -3,7 +3,7 @@ from .setting_utils import _init_app_db
 
 # MySQL configuration
 DATABASE = {
-    'MYSQL_HOST': 'db.michaeldpalmer.com',
+    'MYSQL_HOST': 'mysql',
     'MYSQL_DB': 'hotel',
     'MYSQL_USER': 'hotel',
     'MYSQL_PASSWORD': 'ZfDh3wpDRBZbmUmC'
